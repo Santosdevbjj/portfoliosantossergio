b72cc33ef5596eba4902c100f9bbc18a7e1d0cb0
Sergio <santossergiorealbjj@outlook.com>
2026-01-29 18:26:40 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-// src/app/page.tsx
-import { cookies, headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default function RootPage() {
-  const lang = cookies().get("lang")?.value ?? "pt";
-  redirect(`/${lang}`);
+  redirect("/pt");
 }
