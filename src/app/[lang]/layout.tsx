9d8441b17268d256e64d7b10cc58f3691de04725
Sergio <santossergiorealbjj@outlook.com>
2026-02-08 20:18:54 -0300

    Update layout.tsx

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -9,7 +9,7 @@
 import type { Metadata, Viewport } from 'next'
 import { Inter, Montserrat } from 'next/font/google'
 import Script from 'next/script'
-import '@/app/globals.css'
+import '../globals.css'
 
 import { ThemeProvider } from '@/components/ThemeToggle'
 import { CookieBanner } from '@/components/CookieBanner'
