bc8c59d4f011c587045e6d13336e48f5b4d93df5
Sergio <santossergiorealbjj@outlook.com>
2026-02-18 14:57:18 -0300

    Update error-dictionary.ts

--- src/types/error-dictionary.ts
+++ src/types/error-dictionary.ts
@@ -1,16 +1,18 @@
 export interface ErrorDetail {
-  readonly title: string
-  readonly message: string
-  readonly action: string
+  readonly title: string;
+  readonly message: string;
+  readonly action: string;
 }
 
 export interface ErrorDictionary {
-  readonly InternalServerError: ErrorDetail
-  readonly NotFoundError: ErrorDetail
-  readonly ValidationError: ErrorDetail
-  readonly UnauthorizedError: ErrorDetail
-  readonly ForbiddenError: ErrorDetail
-  readonly TooManyRequestsError: ErrorDetail
-  readonly UnprocessableEntityError: ErrorDetail
-  readonly MethodNotAllowedError: ErrorDetail
+  readonly InternalServerError: ErrorDetail;
+  readonly NotFoundError: ErrorDetail;
+  readonly ValidationError: ErrorDetail;
+  readonly UnauthorizedError: ErrorDetail;
+  readonly ForbiddenError: ErrorDetail;
+  readonly TooManyRequestsError: ErrorDetail;
+  readonly UnprocessableEntityError: ErrorDetail;
+  readonly MethodNotAllowedError: ErrorDetail;
 }
+
+export type ErrorKey = keyof ErrorDictionary;
