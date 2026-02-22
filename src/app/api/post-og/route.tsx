0f280c43b4ddd8d728dce806194bd83e92cceea4
Sergio <santossergiorealbjj@outlook.com>
2026-02-22 18:28:31 -0300

    Update route.tsx

--- src/app/api/post-og/route.tsx
+++ src/app/api/post-og/route.tsx
@@ -1,7 +1,7 @@
 import { ImageResponse } from 'next/og'
 import type { NextRequest } from 'next/server'
 
-// export const runtime = 'edge'
+export const runtime = 'edge'
 
 // -------------------- Helpers --------------------
 
