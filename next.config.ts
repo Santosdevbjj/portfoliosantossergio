ef68351b15dd3a0992b62751fc15e0b8ce4548b8
Sergio <santossergiorealbjj@outlook.com>
2026-02-25 22:20:56 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // reactCompiler: true,
   poweredByHeader: false,
   compress: true,
   typedRoutes: true,
