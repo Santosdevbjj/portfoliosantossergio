276dc3f6caccad8e3cf6ada9ef12357df674460f
Sergio <santossergiorealbjj@outlook.com>
2026-02-26 11:02:41 -0300

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,10 @@
-export default {
+/** @type {import('postcss-load-config').Config} */
+const config = {
   plugins: {
+    // Na v4, este plugin gerencia tudo: detecção de arquivos, 
+    // processamento de variáveis CSS e autoprefixer.
     "@tailwindcss/postcss": {},
   },
 };
+
+export default config;
