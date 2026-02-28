b1b6d90078fc3e240369d2b0291f70ad7066e8ed
Sergio <santossergiorealbjj@outlook.com>
2026-02-28 00:55:49 -0300

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,14 @@
+import './globals.css'
+import { ThemeProvider } from '@/components/ThemeToggle'
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body>
+        <ThemeProvider>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
