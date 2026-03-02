675f8c5221e6e27b8c81924a84cfe5d2aa285a8b
Sergio <santossergiorealbjj@outlook.com>
2026-03-02 18:11:29 -0300

    Update mdx-components.tsx

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,26 +1,19 @@
-import type { MDXComponents } from "mdx/types";
-import Image from "next/image";
-import type { ImageProps } from "next/image";
+import type { MDXComponents } from 'mdx/types'
 
-const components: MDXComponents = {
-  h1: ({ children }) => (
-    <h1 className="text-4xl font-bold my-4">{children}</h1>
-  ),
-  h2: ({ children }) => (
-    <h2 className="text-3xl font-semibold my-3">{children}</h2>
-  ),
-  p: ({ children }) => (
-    <p className="text-base leading-7 my-2">{children}</p>
-  ),
-  img: (props) => (
-    <Image
-      sizes="100vw"
-      style={{ width: "100%", height: "auto" }}
-      {...(props as ImageProps)}
-    />
-  ),
-};
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return {
+    // Permite estilizar títulos, links, etc.
+    h1: ({ children }) => <h1 className="text-4xl font-bold my-4">{children}</h1>,
+    h2: ({ children }) => <h2 className="text-3xl font-semibold my-3">{children}</h2>,
+    p: ({ children }) => <p className="text-base leading-7 my-2">{children}</p>,
+    ...components,
+  }
+} 
 
-export function useMDXComponents(): MDXComponents {
-  return components;
-}
+
+
+
+
+
+
+
