03fb89a84cadef7893a00a58c227f3cf8e29ab7c
Sergio <santossergiorealbjj@outlook.com>
2026-03-11 02:15:09 -0300

    Create twitter-image.tsx

---
+++ src/app/twitter-image.tsx
@@ -0,0 +1,42 @@
+import { ImageResponse } from "next/og"
+
+export const runtime = "edge"
+
+export const size = {
+  width: 1200,
+  height: 630,
+}
+
+export const contentType = "image/png"
+
+export default function Image() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          background: "#0f172a",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          padding: "80px",
+          color: "white",
+        }}
+      >
+        <h1 style={{ fontSize: 70, fontWeight: 700 }}>
+          Sergio Santos
+        </h1>
+
+        <p style={{ fontSize: 34, marginTop: 20 }}>
+          Software Engineer
+        </p>
+
+        <p style={{ fontSize: 26, marginTop: 40 }}>
+          GitHub • LinkedIn • Portfolio
+        </p>
+      </div>
+    ),
+    size
+  )
+}
