fd322592428f1423aab119d21a891cae478bd985
Sergio <santossergiorealbjj@outlook.com>
2026-03-15 00:44:53 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,24 @@
 // tailwind.config.ts
 import type { Config } from 'tailwindcss'
 
-export default {
+const config: Config = {
+  // O darkMode 'class' permite que o prose-invert funcione
+  // quando você alternar o tema do sistema
+  darkMode: 'class',
+  
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}"// Garante que o src/ seja scaneado
-  ], theme: {
-    extend: {}}, plugins: []} satisfies Config
+    "./src/**/*.{js,ts,jsx,tsx,mdx}" // Garante que o src/ seja scaneado
+  ],
+  
+  theme: {
+    extend: {
+      // Aqui você pode adicionar personalizações extras para o 'prose' se desejar
+    },
+  },
+  
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
+
+export default config
